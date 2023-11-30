222aa7ffbcace5134b4b7d4af451f90df942d8c1
azara <yyushahu02@gmail.com>
2023-11-30 22:37:29 +0000

    add func

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -10,6 +10,8 @@ def usage():
         print('Unknown operator. Available operators: +, -, *, /')
         sys.exit(1)
 
+
+def print_calc():
     a = int(sys.argv[1])
     b = int(sys.argv[3])
     if sys.argv[2] == '+':
@@ -26,3 +28,4 @@ if __name__ == '__main__':
     import sys
     from calculator_1 import add, sub, mul, div
     usage()
+    print_calc()
