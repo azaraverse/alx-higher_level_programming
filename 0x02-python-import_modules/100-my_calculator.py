0f8b581fe3e664453b577443b025638a62a1894d
azara <yyushahu02@gmail.com>
2023-12-01 16:48:33 +0000

    re structure

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,17 +1,11 @@
 #!/usr/bin/python3
-
-def usage():
+def print_calc():
     argv_len = len(sys.argv)
     operator = ['+', '-', '*', '/']
     if argv_len != 4:
         print('Usage: {:s} <a> <operator> <b>'.format(sys.argv[0]))
         exit(1)
-    if sys.argv[2] not in operator:
-        print('Unknown operator. Available operators: +, -, *, and /')
-        exit(1)
 
-
-def print_calc():
     a = int(sys.argv[1])
     b = int(sys.argv[3])
     if sys.argv[2] == '+':
@@ -20,12 +14,15 @@ def print_calc():
         print('{:d} - {:d} = {:d}'.format(a, b, sub(a, b)))
     elif sys.argv[2] == '*':
         print('{:d} * {:d} = {:d}'.format(a, b, mul(a, b)))
-    else:
+    elif sys.argv[2] == '/':
         print('{:d} / {:d} = {:.0f}'.format(a, b, div(a, b)))
+    else:
+        if sys.argv[2] not in operator:
+            print('Unknown operator. Available operators: +, -, *, and /')
+            exit(1)
 
 
 if __name__ == '__main__':
     import sys
     from calculator_1 import add, sub, mul, div
-    usage()
     print_calc()
