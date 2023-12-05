a34c973ef408a95b045ccdafa8d74c43d1df3534
Yushahu Yussif <yyushahu02@gmail.com>
2023-12-05 13:57:10 +0000

    rm else condition

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -4,5 +4,4 @@ def multiple_returns(sentence):
         length = len(sentence)
         if sentence == 0:
             return 0, None
-        else:
-            return length, sentence[0]
+        return length, sentence[0]
