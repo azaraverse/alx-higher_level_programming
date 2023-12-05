f07ce02960ef409eddff744cc85cebb31348843e
Yushahu Yussif <yyushahu02@gmail.com>
2023-12-05 10:43:52 +0000

    enhance

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for element in row:
-            print('{:d}'.format(element), end=' ')
+        for element in range(0, len(row)):
+            print('{:d}'.format(row[element]), end='')
+            if element != len(row) - 1:
+                print(' ', end='')
         print()
