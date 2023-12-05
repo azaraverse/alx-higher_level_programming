19d4c8053b2d0f5ceab535cfe483dec2ca0d6258
Yushahu Yussif <yyushahu02@gmail.com>
2023-12-05 11:29:51 +0000

    tuple addition

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    if len(tuple_a) == 0:
+        tup_a = (0, 0)
+    elif len(tuple_a) == 1:
+        tup_a = (tuple_a[0], 0)
+    else:
+        tup_a = tuple_a
+    
+    if len(tuple_b) == 0:
+        tup_b = (0, 0)
+    elif len(tuple_b) == 1:
+        tup_b = (tuple_b[0], 0)
+    else:
+        tup_b = tuple_b
+
+    return (tup_a[0] + tup_b[0], tup_a[1] + tup_b[1])
