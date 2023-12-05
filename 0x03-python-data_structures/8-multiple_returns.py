3364aa28582dfab5a9ed377afbf09d4b972488cf
Yushahu Yussif <yyushahu02@gmail.com>
2023-12-05 14:05:14 +0000

    change syntax

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if sentence:
-        length = len(sentence)
-        if sentence == '':
-            return length, None
-        return length, sentence[0]
+    length = len(sentence)
+    if sentence == '':
+        return length, None
+    return length, sentence[0]
