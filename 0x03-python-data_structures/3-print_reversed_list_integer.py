e7635d94ffd6041ac821d738f97e1f416716f602
Yushahu Yussif <yyushahu02@gmail.com>
2023-12-05 11:28:40 +0000

    efficient reverse

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     if my_list:
-        my_list.reverse()
-        for i in my_list:
-            print('{:d}'.format(i))
+        for element in my_list[::-1]:
+            print('{:d}'.format(element))
+    else:
+        return
