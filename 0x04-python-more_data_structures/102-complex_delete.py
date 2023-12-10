ff2bb7c6a579dbe0c9627547091944847b4b52d8
Yushahu Yussif <yyushahu02@gmail.com>
2023-12-10 23:05:33 +0000

    complex delete key

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def complex_delete(a_dictionary, value):
+    if a_dictionary:
+        values = list(
+            filter(lambda *x: x[0][1] == value, a_dictionary.items())
+            )
+
+        for key, value in values:
+            del a_dictionary[key]
+        return a_dictionary
