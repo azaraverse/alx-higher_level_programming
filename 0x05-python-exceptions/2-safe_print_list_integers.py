59b57e503834f300f8899b3976c5e5da8aa7f3f8
Yushahu Yussif <yyushahu02@gmail.com>
2023-12-18 23:37:34 +0000

    list int

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    integer_count = 0
+
+    try:
+        for i in range(x):
+            # check if the element is an integer
+            if isinstance(my_list[i], int):
+                # print the integer on the same line
+                print("{:d}".format(my_list[i]), end='')
+
+                # increment the count of int printed to move to the next
+                integer_count += 1
+    except IndexError:
+        # if an index error occurs, we have reached the end of our list
+        pass
+    # handle new line
+    print()
+
+    # return the real number of integers printed
+    return (integer_count)
