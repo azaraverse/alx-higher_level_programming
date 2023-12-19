2dd3e6e4440fd05ddaa766aa160787a44e197ce8
Yushahu Yussif <yyushahu02@gmail.com>
2023-12-19 21:27:46 +0000

    update file

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -2,7 +2,13 @@
 
 
 class Square:
-    """
-    An empty class Square that defines a square.
-    """
-    pass
+    """An empty class Square that defines a square."""
+
+    def __init__(self) -> None:
+        """Creates an instance of the Square class."""
+        pass
+
+
+    def __str__(self) -> str:
+        """Returns a string representation of the Square object."""
+        return "Square class instance"
