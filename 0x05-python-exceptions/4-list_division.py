d381415c0cdc6b5247189129b89121e50e999424
Yushahu Yussif <yyushahu02@gmail.com>
2023-12-19 01:02:58 +0000

    separate div

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,32 +1,32 @@
 #!/usr/bin/python3
+def division(a, b):
+    # init res with 0
+    res = 0
+
+    try:
+        # try division
+        res = a / b
+        # handle 0 division error
+    except ZeroDivisionError:
+        print("division by 0")
+        # handle type error
+    except TypeError:
+        print("wrong type")
+    finally:
+        # return result of division
+        return res
+
+
 def list_division(my_list_1, my_list_2, list_length):
     res_list = []
 
     for i in range(list_length):
         try:
             element1 = my_list_1[i]
-            element2 = my_list_2[i] if i < len(my_list_2) else None
+            element2 = my_list_2[i]
 
-            # check if both elements are numbers
-            # if not (isinstance(element1, (int, float))
-            # and isinstance(element2, (int, float))):
-            # raise TypeError("wrong type")
-
-            # check if the divisor is not zero
-            if element2 == 0:
-                raise ZeroDivisionError("division by 0")
-
-            # do division and append result to the list
-            res_list.append(element1 / element2)
-
-        except TypeError:
-            print("wrong type")
-            res_list.append(0)
-        except ZeroDivisionError as err:
-            print(err)
-            res_list.append(0)
+            res_list.append(division(element1, element2))
         except IndexError:
             print("out of range")
             res_list.append(0)
-    # return the list
     return (res_list)
