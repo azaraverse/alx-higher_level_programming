a0f22229d65d685f40b6f8c26821b2d25120f5d4
Yushahu Yussif <yyushahu02@gmail.com>
2023-12-19 23:54:28 +0000

    type error redo

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -72,9 +72,9 @@ class Square:
         """
         # handle '#' and position when size is greater than 0
         if self.__size > 0:
-            for i in range(self.__position[1]):
+            for _ in range(self.__position[1]):
                 print()
-            for i in range(self.__size):
+            for _ in range(self.__size):
                 print(' ' * self.__position[0] + '#' * self.__size)
         else:
             print()
@@ -96,9 +96,9 @@ class Square:
         Args (int):
             value: position to set.
         """
-        if not (isinstance(value, tuple) or len(value) != 2 or not all(
-            isinstance(i, int) for i in value) or any(i < 0 for i in value)
-        ):
+        if not isinstance(value, tuple) or len(value) != 2:
+            raise TypeError('position must be a tuple of 2 positive integers')
+        if sum(1 for i in value if isinstance(i, int) and i >= 0) != 2:
             raise TypeError('position must be a tuple of 2 positive integers')
         else:
             self.__position = value
