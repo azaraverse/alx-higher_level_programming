f44c452b269278deecc2b382a97763cad2c0272f
Yushahu Yussif <yyushahu02@gmail.com>
2024-01-04 00:41:52 +0000

    first tdd init

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+A function that adds 2 integers.
+"""
+
+
+def add_integer(a, b=98):
+    """Adds two integers
+    Returns: An integer; the addition of a and b.
+    """
+    if not isinstance(a, int) and not isinstance(a, float):
+        raise TypeError('a must be an integer')
+    if not isinstance(b, int) and not isinstance(b, float):
+        raise TypeError('b must be an integer')
+
+    if isinstance(a, float):
+        a = int(a)
+    if isinstance(b, float):
+        b = int(b)
+
+    return a + b
---
+++ 0x07-python-test_driven_development/README.md
@@ -0,0 +1,3 @@
+# TEST DRIVEN DEVELOPMENT
+This directory contains projects and tasks that tests the understanding
+of TDD. It focuses on the use of with doctests or unittests.
