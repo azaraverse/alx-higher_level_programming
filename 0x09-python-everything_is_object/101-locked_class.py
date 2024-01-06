e37067cf30e63e30525db2fd018cf44e59c8d9ce
Yushahu Yussif <yyushahu02@gmail.com>
2024-01-06 09:03:16 +0000

    class doc and moule doc

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
+"""
+An empty class
+"""
 
 
 class LockedClass:
+    """Empty class but prevents user from dynamically creating new instance
+    attributes, execpt it is called 'first_name'
+    """
     __slots__ = ['first_name']
 
     # def __init__(self, first_name) -> None:
