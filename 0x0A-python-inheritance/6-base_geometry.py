330779d1ab8200817f9a8fb17ca89c2ef2de6568
Yushahu Yussif <yyushahu02@gmail.com>
2024-01-08 20:33:48 +0000

    improve geo

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Class BaseGeometry
+"""
+
+
+class BaseGeometry():
+    """Class BaseGeometry"""
+    def __init__(self) -> None:
+        """Init instance created."""
+        pass
+
+    def area(self):
+        """area function: empty
+
+        Raises:
+            Exception: No implementation
+        """
+        raise Exception('area() is not implemented')
