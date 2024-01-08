cff65bcb22fc6ce5571bc160c5879fdb8df8eb34
Yushahu Yussif <yyushahu02@gmail.com>
2024-01-08 20:21:31 +0000

    del else:

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -18,5 +18,4 @@ def inherits_from(obj, a_class):
     if issubclass(type(obj), a_class):
         if type(obj) is not a_class:
             return True
-    else:
-        return False
+    return False
