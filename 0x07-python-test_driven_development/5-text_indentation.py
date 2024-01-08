4f69afa7ee7a42e75e2c359501d7015838a4c37e
Yushahu Yussif <yyushahu02@gmail.com>
2024-01-08 23:15:56 +0000

    pycodestyle adhere

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -15,7 +15,7 @@ def text_indentation(text):
     """
     if not isinstance(text, str):
         raise TypeError('text must be a string')
-    
+
     punctuation_set = {'.', '?', ':'}
     current_line = ''
 
