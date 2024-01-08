53f2fa815c432d8074d3a08afcc31a0a6d8a4a8f
Yushahu Yussif <yyushahu02@gmail.com>
2024-01-08 21:50:45 +0000

    change from func to import

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,36 +1,5 @@
 #!/usr/bin/python3
-"""Class BaseGeometry
-"""
-
-
-class BaseGeometry():
-    """Class BaseGeometry"""
-    def area(self):
-        """area function: empty
-
-        Raises:
-            Exception: No implementation
-        """
-        raise Exception('area() is not implemented')
-
-    def integer_validator(self, name, value):
-        """integer_validator function
-
-        Args:
-            name: arg1
-            value: arg2
-
-        Raises:
-            TypeError: if value is not an integer
-            ValueError: if value is less than or equal to 0
-        """
-        # validate value is an integer
-        if type(value) is not int:
-            raise TypeError(f'{name} must be an integer')
-
-        # validate value is greater than 0
-        if value <= 0:
-            raise ValueError(f'{name} must be greater than 0')
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
