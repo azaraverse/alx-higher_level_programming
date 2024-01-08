7004fb46c2399abce5ad3abde8f94d5286642a36
Yushahu Yussif <yyushahu02@gmail.com>
2024-01-08 22:18:18 +0000

    pycodestyle adhere

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -10,7 +10,7 @@ class MyInt(int):
     def __eq__(self, __value: object) -> bool:
         """__eq__ will be inverted to return __ne__"""
         return super().__ne__(__value)
-    
+
     def __ne__(self, __value: object) -> bool:
         """__ne__ will be inverted to return __eq__"""
         return super().__eq__(__value)
