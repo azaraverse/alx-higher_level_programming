a122c32a25fcbb698a330140a5e8e883a0c957b2
Yushahu Yussif <yyushahu02@gmail.com>
2024-01-09 23:27:28 +0000

    whitespace

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -8,7 +8,7 @@ import json
 def save_to_json_file(my_obj, filename):
     """A function that writes an obj to a text file,
     using a JSON representation
-    
+
     Args:
         my_obj (object): object to write to JSON file
         filename (str): file name of JSON file
--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -7,7 +7,7 @@ import json
 
 def load_from_json_file(filename):
     """A function that creates an obj from a JSON file.
-    
+
     Args:
         filename (str): file name of JSON file
     Returns:
