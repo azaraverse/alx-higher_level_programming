8692cea9b91ba52b490f4a8b9efbe2388ba3e4b7
Yushahu Yussif <yyushahu02@gmail.com>
2024-01-10 01:14:08 +0000

    pascals triangle

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+
+def pascal_triangle(n):
+    """generate pascal's triangle up to the specified level n
+
+    Args:
+        n (int): the level of Pascal's triangle
+    Returns:"
+        a list of lists representing Pascal's triangle
+    """
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+        row.append(1)
+        triangle.append(row)
+    return triangle
