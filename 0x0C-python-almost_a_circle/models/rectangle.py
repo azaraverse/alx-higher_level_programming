04e59e83f8acdb5fe346150b05e00cc17dee6e10
Yushahu Yussif <yyushahu02@gmail.com>
2024-01-12 22:07:12 +0000

    update with *args variable arguments

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -115,3 +115,20 @@ class Rectangle(Base):
             f'[Rectangle] ({self.id}) '
             f'{self.x}/{self.y} - {self.width}/{self.height}'
         )
+    
+    def update(self, *args):
+        """Assigns arguments to id, width, height, x and y attrs.
+        
+        Args:
+            *args: variable number of arguments
+        """
+        if len(args) >= 1:
+            self.id = args[0]
+        if len(args) >= 2:
+            self.width = args[1]
+        if len(args) >= 3:
+            self.height = args[2]
+        if len(args) >= 4:
+            self.x = args[3]
+        if len(args) >= 5:
+            self.y = args[4]
