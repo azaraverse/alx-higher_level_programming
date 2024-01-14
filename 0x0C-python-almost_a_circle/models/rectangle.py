49772cf70af7528674dba4639e1e7e1fe45d451f
Yushahu Yussif <yyushahu02@gmail.com>
2024-01-14 15:24:47 +0000

    handle to dictionary that prints a dictionary for rectangle class

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -141,3 +141,13 @@ class Rectangle(Base):
         else:
             for key, value in kwargs.items():
                 setattr(self, key, value)
+
+    def to_dictionary(self):
+        """Return the dictionary representation of Rectangle"""
+        return {
+            'id': self.id,
+            'width': self.width,
+            'height': self.height,
+            'x': self.x,
+            'y': self.y
+        }
