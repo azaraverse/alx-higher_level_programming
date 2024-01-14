27796c70754542671c0a46315bfeda683835f18c
Yushahu Yussif <yyushahu02@gmail.com>
2024-01-14 16:20:15 +0000

    include string representation of dict

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ DEFINES BASE CLASS
 """
+import json
 
 
 class Base():
@@ -18,3 +19,17 @@ class Base():
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Static method that returns the JSON string representation of
+        list_dictionaries
+
+        Args:
+            list_dictionaries (list): list of dictionaries.
+        Returns:
+            JSON string representation of the list of dictionaries
+        """
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return '[]'
+        return json.dumps(list_dictionaries)
