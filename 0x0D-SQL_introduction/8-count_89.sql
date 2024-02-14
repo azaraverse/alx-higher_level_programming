f20bb5216dfafb0176d31c3cf9d4c9a0d9171cf3
root <root@DESKTOP-47NMB5A>
2024-02-14 01:22:54 +0000

    full, list, insertion and record count in sql

---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,2 @@
+-- a script that displays the number of roecords with (id=89) in the table; first_table, of the database; hbtn_0c_0 in a MySQL server
+SELECT COUNT(*) FROM first_table WHERE id = 89;
