bc3e7480f62c26388189c3b13fdaece10c0d3a56
Yushahu Azara <yyushahu02@gmail.com>
2024-02-15 07:16:52 +0000

    states table that auto increases an auto generated id

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,9 @@
+-- a script that creates a database from the specified name and a table into the database from the specified name on a MySQL server
+-- create the database
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+-- specify database to use for table creation
+USE hbtn_0d_usa;
+
+-- create the table
+CREATE TABLE IF NOT EXISTS states (id INT PRIMARY KEY NOT NULL AUTO_INCREMENT, name VARCHAR(256) NOT NULL);
