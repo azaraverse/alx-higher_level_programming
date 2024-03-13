47c0809cdd76181f24325ecd0f2c3c001456268e
Azara <yyushahu02@gmail.com>
2024-03-13 00:47:08 +0000

    count number of times an item occurs in a list

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+exports.nbOccurences = function (list, searchElement) {
+    let i = 0;
+    let kidaya = 0;
+    while (i < list.length) {
+        if (list[i] === searchElement) {
+            kidaya++;
+        }
+        i++;
+    }
+    return kidaya;
+};
