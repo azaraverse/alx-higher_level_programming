af5b507296b6755ccd47cfbc80277c35b0dc8bff
Azara <yyushahu02@gmail.com>
2024-03-13 00:48:29 +0000

    make code semistandard compliant

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
 
 exports.nbOccurences = function (list, searchElement) {
-    let i = 0;
-    let kidaya = 0;
-    while (i < list.length) {
-        if (list[i] === searchElement) {
-            kidaya++;
-        }
-        i++;
+  let i = 0;
+  let kidaya = 0;
+  while (i < list.length) {
+    if (list[i] === searchElement) {
+      kidaya++;
     }
-    return kidaya;
+    i++;
+  }
+  return kidaya;
 };
