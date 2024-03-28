760e181e6d04b11d62aa61cfa258c871e08b2ee0
Azara <yyushahu02@gmail.com>
2024-03-28 17:02:53 +0000

    use f string formatting

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -9,6 +9,6 @@ if __name__ == '__main__':
     ) as response:
         html = response.read()
         print('Body Response:')
-        print('\t- type:', type(html))
-        print('\t- content:', html)
-        print('\t- utf8 content:', html.decode('utf-8'))
+        print(f'\t- type: {type(html)}')
+        print(f'\t- content: {html}')
+        print(f'\t- utf8 content: {html.decode("utf-8")}')
