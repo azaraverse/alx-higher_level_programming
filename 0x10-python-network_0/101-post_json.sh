ead968b3e47f4161f25d7e79e9f036fb3d55dab7
Yushahu <yyushahu02@gmail.com>
2024-03-27 18:38:55 -0700

    include application as content-type caveat

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends a json POST request to a url and displays the body of the response. uses a file to send POST request
-curl -sX POST -H "content-type: json" "$1" --data @"$2"
+curl -sX POST -H "content-type: application/json" "$1" --data @"$2"
