d313325308ee28692b7aae772469daafd6575ab1
Azara <yyushahu02@gmail.com>
2024-03-28 16:58:46 +0000

    include print statements within with statement

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
         'https://alx-intranet.hbtn.io/status'
     ) as response:
         html = response.read()
-    print('Body Response:')
-    print('\t- type:', type(html))
-    print('\t- content:', html)
-    print('\t- utf8 content:', html.decode('utf-8'))
+        print('Body Response:')
+        print('\t- type:', type(html))
+        print('\t- content:', html)
+        print('\t- utf8 content:', html.decode('utf-8'))
