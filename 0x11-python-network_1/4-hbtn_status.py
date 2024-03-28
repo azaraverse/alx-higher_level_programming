7347bf75ecd1c6c269619d11fb69adf3e5b0f0b1
Azara <yyushahu02@gmail.com>
2024-03-28 17:30:10 +0000

    delete whitespaces

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -8,5 +8,5 @@ if __name__ == '__main__':
     url_link = 'https://alx-intranet.hbtn.io/status'
     r = requests.get(url_link)
     print('Body response:')
-    print(f'\t - type: {type(r.text)}')
-    print(f'\t - content: {r.text}')
+    print(f'\t- type: {type(r.text)}')
+    print(f'\t- content: {r.text}')
