eb99a1235a952cb1a3e4bf77a4d9ea4d97e18951
Yushahu <yyushahu02@gmail.com>
2024-03-27 17:03:38 -0700

    enhance HTTP allowed HEADER request

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # a bash script that takes in the URL passed as an argument and displays all HTTP methods the server will accept
-curl -s --head "$1" | grep -i Allow | awk '{print $2, $3, $4}'
+curl -s --head -X OPTIONS "$1" | grep -i '^allow' | awk '{print substr($0, index($0,$2))}'
