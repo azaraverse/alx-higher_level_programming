7934542ba241d847aeb1f751dd47966ae8be5da1
Azara <yyushahu02@gmail.com>
2024-03-28 17:16:14 +0000

    re spell response in lower case

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
     url_link = 'https://alx-intranet.hbtn.io/status'
     with urllib.request.urlopen(url_link) as response:
         html = response.read()
-        print('Body Response:')
+        print('Body response:')
         print(f'\t- type: {type(html)}')
         print(f'\t- content: {html}')
         print(f'\t- utf8 content: {html.decode("utf-8")}')
