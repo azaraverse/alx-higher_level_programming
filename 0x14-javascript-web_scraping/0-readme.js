5eea8f99d1043c67265b3fa62da597d2ff922eb7
Azara <yyushahu02@gmail.com>
2024-04-23 08:16:14 +0000

    add javascript to read and write file content to console

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+// create fileSystem instance
+const fs = require('fs');
+
+// write a function to read and print file content to console
+function readPrintFile (filePath) {
+  fs.readFile(filePath, 'utf-8', (data, err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(data);
+    }
+  });
+}
+
+const filePath = process.argv[2];
+readPrintFile(filePath);
---
+++ 0x14-javascript-web_scraping/README.md
@@ -0,0 +1,2 @@
+# Javascript - Web Scraping
+This directory contains javascript tasks on web scraping.
