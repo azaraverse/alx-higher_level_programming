aba13f8d92130a238cf578c07eb461fa85170161
Azara <yyushahu02@gmail.com>
2024-04-23 20:14:00 +0000

    request webpage content from loripsum.net and store content in a file

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+
+// create fileSystem instance
+const fs = require('fs');
+const request = require('request');
+
+// write a function to write string to file
+function writeWebPage (urlPath, filePath) {
+    request.get(urlPath, (err, response, body) => {
+        if (err) {
+            console.log(err);
+        } if (response.statusCode !== 200) {
+            console.log('code:', response.statusCode)
+        } else {
+            fs.writeFile(filePath, body, 'utf-8', (err) => {
+                if (err) {
+                  console.log(err);
+                }
+              });
+        }
+    });
+}
+
+const urlPath = process.argv[2];
+const filePath = process.argv[3];
+writeWebPage(urlPath, filePath);
